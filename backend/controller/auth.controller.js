d0bce3db26089cb1ac035a46354e84b9813406c8
GARajab <mohamed.rajab@ewa.com>
2025-01-05 22:58:49 +0300

    email ok

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,76 +1,76 @@
-import { User } from "../models/user.model.js";
-import bcryptjs from "bcryptjs";
-import generateTokenAndSetCookie from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail } from "../mailtrap/emails.js";
+import { User } from "../models/user.model.js"
+import bcryptjs from "bcryptjs"
+import generateTokenAndSetCookie from "../utils/generateTokenAndSetCookie.js"
+import { sendVerificationEmail } from "../mailtrap/emails.js"
 
 export const signup = async (req, res) => {
-  const { name, email, password } = req.body;
-  const SALT = process.env.SALT;
+  const { name, email, password } = req.body
+  const SALT = process.env.SALT
   try {
     if (!name || !email || !password) {
-      return res.status(400).json({ error: "Please fill all the fields" });
+      return res.status(400).json({ error: "Please fill all the fields" })
     }
-    const userInDB = await User.findOne({ email });
+    const userInDB = await User.findOne({ email })
     if (userInDB) {
-      return res.status(400).json({ error: "User already exists" });
+      return res.status(400).json({ error: "User already exists" })
     }
-    const hashedPassword = await bcryptjs.hash(password, 10);
+    const hashedPassword = await bcryptjs.hash(password, 10)
     const verificationToken = Math.floor(
       100000 + Math.random() * 900000
-    ).toString();
+    ).toString()
     const user = new User({
       name,
       email,
       password: hashedPassword,
       verificationToken,
       verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
-    });
-    await user.save(); // may ask to make it user.save()
-    generateTokenAndSetCookie(res, user._id);
-    await sendVerificationEmail(user.email, verificationToken);
+    })
+    await user.save() // may ask to make it user.save()
+    generateTokenAndSetCookie(res, user._id)
+    await sendVerificationEmail(user.email, verificationToken)
     res.status(201).json({
       success: true,
       message: "User created successfully",
       user: { ...user._doc, password: undefined },
-    });
+    })
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Internal server error" });
+    console.log(error)
+    res.status(500).json({ error: "Internal server error" })
   }
-};
+}
 
 export const verifyEmail = async (req, res) => {
-  const { code } = req.body;
+  const { code } = req.body
   try {
     const user = await User.findOne({
       verificationToken: code,
       verificationTokenExpiresAt: { $gt: Date.now() },
-    });
+    })
     if (!user)
       return res.status(400).json({
         success: false,
         message: "Invalid Or Expired Verification Code",
-      });
-    user.isVerified = true;
-    user.verificationToken = undefined;
-    user.verificationTokenExpiresAt = undefined;
-    await user.save();
-    await sendWelcomeEmail(user.email, user.name);
+      })
+    user.isVerified = true
+    user.verificationToken = undefined
+    user.verificationTokenExpiresAt = undefined
+    await user.save()
+    await sendWelcomeEmail(user.email, user.name)
     res.status(200).json({
       success: true,
       message: "Email Verified Successfuly!",
       user: {
         ...user._doc,
         password: undefined,
-      },
-    });
+      },//now on 1:16:57 and 1:08:37
+    })
   } catch (error) {}
-};
+}
 
 export const signin = async (req, res) => {
-  res.send("Signin");
-};
+  res.send("Signin")
+}
 export const signout = async (req, res) => {
-  res.send("Signout");
-};
+  res.send("Signout")
+}
 //i have to make the signup and signin and signout functions
--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,21 +1,22 @@
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
-import { mailtrapClient, sender } from "./mailtrap.config.js";
+import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js"
+import { mailtrapClient, sender } from "./mailtrap.config.js"
 
 export const sendVerificationEmail = async (email, name) => {
-  const recipient = [{ email }];
+  const recipient = [{ email }]
   try {
     await mailtrapClient.send({
       from: sender,
       to: recipient,
-      subject: "Verify Your Email",
-      template_uuid: "577ffd4f-ad9e-4440-a502-fe60b51ca1c9",
-      template_variables: {
-        name: name,
-        company_info_name: "iRajab",
-      },
-    });
-    console.log("Email Sent Successfully", responce);
+      // template_uuid: "577ffd4f-ad9e-4440-a502-fe60b51ca1c9",
+      // template_variables: {
+      //   name: name,
+      //   company_info_name: "iRajab",
+      // },
+    })
+    console.log("Email Sent Successfully")
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
+
+export sendWelcomeEmail = async (email, name) => {
