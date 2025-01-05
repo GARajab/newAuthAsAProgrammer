722f1d19a28855c40692e43487a878ad20fc3abb
GARajab <mohamed.rajab@ewa.bh>
2025-01-05 16:59:33 +0300

    doing the welcome email

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,15 @@
-import express from "express"
-import { signin, signout, signup } from "../controller/auth.controller.js"
-const router = express.Router()
+import express from "express";
+import {
+  signin,
+  signout,
+  signup,
+  verifyEmail,
+} from "../controller/auth.controller.js";
+const router = express.Router();
 
-router.post("/signup", signup)
-router.post("/signin", signin)
-router.post("/signout", signout)
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.post("/signout", signout);
+router.post("/verify-email", verifyEmail);
 
-export default router
+export default router;
